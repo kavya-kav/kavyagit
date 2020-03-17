b306fa6c925538424538763fd9f3ec28eef9c233
kavya-kav <62107318+kavya-kav@users.noreply.github.com>
2020-03-17 22:27:14 +0530

    Add files via upload

---
+++ bignum.java
@@ -0,0 +1,19 @@
+public class bignum
+{
+public static void main(String[] args)
+{
+int i=2,j=4,k=3;
+if(i>j && i>k)
+{
+System.out.println(i);
+}
+else if(j>k)
+{
+System.out.println(j);
+}
+else
+{
+System.out.println(k);
+}
+}
+}
