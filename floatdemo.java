30f52fe9d57cf6e76eb93ce067c5ee3d89895cac
kavya-kav <62107318+kavya-kav@users.noreply.github.com>
2020-03-17 22:18:10 +0530

    Add files via upload

---
+++ floatdemo.java
@@ -0,0 +1,11 @@
+class floatdemo
+{
+public static void main(String arg[])
+{
+float n1=5.2f;
+float n2=3.3f;
+float n3=n1+n2;
+System.out.println(n1+n2);
+System.out.println("sum of two floats:"+n3);
+}
+}
