439c8d4ad083821565e9960f297fb7beb516d529
kavya-kav <62107318+kavya-kav@users.noreply.github.com>
2020-03-17 23:18:53 +0530

    Add files via upload

---
+++ switchdemo.java
@@ -0,0 +1,21 @@
+public class switchdemo
+{
+public static void main(String[] args)
+{
+int i=7;
+switch(i)
+{
+case 0:
+System.out.println("zero");
+break;
+case 2:
+System.out.println("two");
+break;
+case 5:
+System.out.println("five");
+break;
+default:
+System.out.println("some number");
+}
+}
+}
