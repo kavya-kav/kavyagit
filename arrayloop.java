fde14c6767f33c0db3c6e760d911ce67ac0da4e5
kavya-kav <62107318+kavya-kav@users.noreply.github.com>
2020-03-18 23:37:03 +0530

    Add files via upload

---
+++ arrayloop.java
@@ -0,0 +1,13 @@
+public class arrayloop
+{
+public static void main(String[] args)
+{
+int n[]=new int[5];
+    for(int i=0;i<5;i++)
+     { 
+          n[i]=i+1;
+      }
+for(int i=0;i<5;i++)
+System.out.println(n[i]);
+}
+}
