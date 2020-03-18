9da8bfedc229a79c73a0baedaf6ec9b58d0e56ee
kavya-kav <62107318+kavya-kav@users.noreply.github.com>
2020-03-18 23:08:21 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,12 @@
+public class swap
+{
+ public static void main(String[] args) 
+    { 
+       int a=3,b=5;
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println("a : " +a);
+System.out.println("b : " +b);
+}
+}
