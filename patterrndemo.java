8b062ea01eb37dcb96996a07ebf7cbfe5c836506
kavya-kav <62107318+kavya-kav@users.noreply.github.com>
2020-03-20 21:28:36 +0530

    Add files via upload

---
+++ patterrndemo.java
@@ -0,0 +1,15 @@
+public class patterndemo
+{
+public static void main(String[] args)
+{
+int i;
+int j;
+for(i=1;i<4;i++)
+{
+for(j=1;j<=i;j++)
+{
+System.out.println(i + " ");
+}
+System.out.println(" ");
+}
+} 
