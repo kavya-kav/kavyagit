74481b6cea9754119d0e9ac445ef6ae0a6b49b9d
kavya-kav <62107318+kavya-kav@users.noreply.github.com>
2020-03-20 23:26:56 +0530

    Add files via upload

---
+++ stringdemo.java
@@ -0,0 +1,8 @@
+public class Stringdemo {
+
+   public static void main(String args[]) {
+      String palindrome = "dog saw i was god";
+      int len = palindrome.length();
+      System.out.println( "String Length is : " + len );
+   }
+}
