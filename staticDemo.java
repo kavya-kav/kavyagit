b329e71a6416d74e693313cec0a0b93d0c69e104
kavya-kav <62107318+kavya-kav@users.noreply.github.com>
2020-03-20 21:30:11 +0530

    Add files via upload

---
+++ staticDemo.java
@@ -0,0 +1,16 @@
+public class staticDemo
+{
+public static void main(String[] args)
+{
+Abc.show();
+}
+}
+class Abc
+{
+static int i=5;
+public static void show()
+{
+System.out.println("hi");
+System.out.println(i);
+}
+}
