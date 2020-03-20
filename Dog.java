4ef9baabf15cb2622874d2c4dedbbf419948d21d
kavya-kav <62107318+kavya-kav@users.noreply.github.com>
2020-03-20 21:23:13 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,10 @@
+public class Dog
+{
+    public void sound(){
+        System.out.println("Bow");
+    }
+    public static void main(String args[]){
+    	     Dog obj  = new Dog();
+    	obj.sound();
+    }
+}
