a3e2368db7cefbe3b6f69e054a0564639fd34d9f
kavya-kav <62107318+kavya-kav@users.noreply.github.com>
2020-03-21 13:07:12 +0530

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,41 @@
+import java.time.*; 
+import java.time.format.DateTimeFormatter; 
+   
+public class Date { 
+  
+public static void LocalDateTimeApi() 
+{ 
+     LocalDate date = LocalDate.now(); 
+    System.out.println("the current date is "+ 
+                        date);
+    LocalTime time = LocalTime.now(); 
+    System.out.println("the current time is "+ 
+                        time); 
+    LocalDateTime current = LocalDateTime.now(); 
+    System.out.println("current date and time : "+ 
+                        current); 
+    DateTimeFormatter format =  
+      DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");   
+    
+    String formatedDateTime = current.format(format);   
+     
+    System.out.println("in foramatted manner "+ 
+                        formatedDateTime); 
+    Month month = current.getMonth(); 
+    int day = current.getDayOfMonth(); 
+    int seconds = current.getSecond(); 
+    System.out.println("Month : "+month+" day : "+ 
+                        day+" seconds : "+seconds); 
+    LocalDate date2 = LocalDate.of(1950,1,26); 
+    System.out.println("the repulic day :"+date2); 
+    LocalDateTime specificDate =  
+        current.withDayOfMonth(24).withYear(2016); 
+  
+    System.out.println("specfic date with "+ 
+                       "current time : "+specificDate); 
+} 
+    public static void main(String[] args)  
+    { 
+        LocalDateTimeApi(); 
+    } 
+} 
